c4c7024caab9a8e54d8768cc5a35b3f88bdc0cc8
soubhagyasethy <ssethy017@gmail.com>
2023-01-15 23:58:06 +0530

    added javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+const search = () => {
+    const searchbox = document.getElementById("search-item");
+    const storeitems = document.querySelector(".product-list");
+    const product = document.querySelectorAll(".product");
+    const pname = document.getElementsByTagName("h2");
+    console.log(searchbox, storeitems, product, pname );
+}
